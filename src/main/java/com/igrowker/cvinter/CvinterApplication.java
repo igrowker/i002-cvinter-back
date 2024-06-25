f0b8eacda8ff4717620a4f9282690878b53e52e2
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2024-06-25 08:58:03 -0300

    Initial Commit
    Spting Initializr, file structure and basic configs

---
+++ src/main/java/com/igrowker/cvinter/CvinterApplication.java
@@ -0,0 +1,13 @@
+package com.igrowker.cvinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igrowker/cvinter/CvinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igrowker.cvinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
