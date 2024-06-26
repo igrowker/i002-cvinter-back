208dea8e8d1f61d7d802beb64230fc1a584f43cc
PerZZi <emiliano.estebandiaz@gmail.com>
2024-06-26 15:51:02 -0300

    clase user

---
+++ src/main/java/com/igrowker/cvinter/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.igrowker.cvinter.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/igrowker/cvinter/models/User.java
@@ -0,0 +1,117 @@
+package com.igrowker.cvinter.models;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String FullName;
+    private String cvUrl;
+    private boolean twoFactorEnabled;
+    private String twoFactorSecret;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+    public User(String email, String password, String fullName, String cvUrl, boolean twoFactorEnabled, String twoFactorSecret, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.email = email;
+        this.password = password;
+        FullName = fullName;
+        this.cvUrl = cvUrl;
+        this.twoFactorEnabled = twoFactorEnabled;
+        this.twoFactorSecret = twoFactorSecret;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getCvUrl() {
+        return cvUrl;
+    }
+
+    public void setCvUrl(String cvUrl) {
+        this.cvUrl = cvUrl;
+    }
+
+    public boolean isTwoFactorEnabled() {
+        return twoFactorEnabled;
+    }
+
+    public void setTwoFactorEnabled(boolean twoFactorEnabled) {
+        this.twoFactorEnabled = twoFactorEnabled;
+    }
+
+    public String getTwoFactorSecret() {
+        return twoFactorSecret;
+    }
+
+    public void setTwoFactorSecret(String twoFactorSecret) {
+        this.twoFactorSecret = twoFactorSecret;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", FullName='" + FullName + '\'' +
+                ", cvUrl='" + cvUrl + '\'' +
+                ", twoFactorEnabled=" + twoFactorEnabled +
+                ", twoFactorSecret='" + twoFactorSecret + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
