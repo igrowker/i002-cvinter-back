1e9a0e8e0e41f9cc0c864977ed1e56c89eded6cf
L.Pol <123669377+LPintos14@users.noreply.github.com>
2024-06-30 12:52:31 -0300

    Create InterviewRepository.java

---
+++ src/main/java/com/igrowker/cvinter/model/repository/InterviewRepository.java
@@ -0,0 +1,11 @@
+package com.igrowker.cvinter.model.repository;
+
+import com.igrowker.cvinter.model.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface InterviewRepository extends MongoRepository<Interview, long>{
+
+    Interview findById(Long id);
+
+    List<Interview> findAll();
+}
