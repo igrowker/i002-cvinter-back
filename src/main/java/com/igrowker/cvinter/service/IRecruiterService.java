f29f7ba08fae11e616e25cce0c2f7b0e40073dc9
L.Pol <123669377+LPintos14@users.noreply.github.com>
2024-06-30 12:12:50 -0300

    Update IRecruiterService.java

--- src/main/java/com/igrowker/cvinter/service/IRecruiterService.java
+++ src/main/java/com/igrowker/cvinter/service/IRecruiterService.java
@@ -1,4 +1,13 @@
+
 package com.igrowker.cvinter.service;
 
+import cvinter.igrowker.model.entity.User;
+import java.util.List;
+
 public interface IRecruiterService {
+
+ List<User> getRecruitedUsersByRecruiterId(String recruiterId);
+
+
+    
 }
