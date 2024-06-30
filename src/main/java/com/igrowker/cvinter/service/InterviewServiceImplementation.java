fad664038468ee9f7113e2eba3a15ff253c9c98e
L.Pol <123669377+LPintos14@users.noreply.github.com>
2024-06-30 13:05:20 -0300

    Update InterviewServiceImplementation.java

--- src/main/java/com/igrowker/cvinter/service/InterviewServiceImplementation.java
+++ src/main/java/com/igrowker/cvinter/service/InterviewServiceImplementation.java
@@ -1,7 +1,31 @@
 package com.igrowker.cvinter.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igrowker.cvinter.model.entity.Interview;
+import com.igrowker.cvinter.model.repository.InterviewRepository;
 
 @Service
 public class InterviewServiceImplementation implements IInterviewService {
+   @Autowired
+    private InterviewRepository interviewRepository;
+
+    @Override
+    public List<Interview> getAllInterviews() {
+        return InterviewRepository.findAll();
+    }
+
+    @Override
+    public List<Interview> findByUserId(Long UserId) {
+        return InterviewRepository.findByUsuarioId(UserId); o
+    }
+
+    @Override
+    public List<Interview> findByRecruiterId(Long RecruiterId) {
+        return InterviewRepository.findByReclutadorId(RecruiterId); 
+    }
+
+  
 }
