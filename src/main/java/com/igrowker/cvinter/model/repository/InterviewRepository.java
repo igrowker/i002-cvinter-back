14cd99b684f8f0c91ceb33f01f52f5d53a91cde1
L.Pol <123669377+LPintos14@users.noreply.github.com>
2024-07-01 13:45:18 -0300

    Update InterviewRepository.java

--- src/main/java/com/igrowker/cvinter/model/repository/InterviewRepository.java
+++ src/main/java/com/igrowker/cvinter/model/repository/InterviewRepository.java
@@ -1,13 +1,15 @@
 package com.igrowker.cvinter.model.repository;
 
 import com.igrowker.cvinter.model.entity.Interview;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface InterviewRepository extends MongoRepository<Interview, long>{
 
-    Interview findById(Long id);
+    Interview findById(Long interviewId);
 
     List<Interview> findAll();
 
-    List<Interview> findByReclutadorId(Long reclutadorId);
+    List<Interview> findByRecruiterId(Long recruiterId);
 }
+
